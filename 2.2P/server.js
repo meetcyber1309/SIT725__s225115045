2ff0bea0cb3cd3a3911126da5d487b2c6851f0c7
meetcyber1309 <meetm51g@gmail.com>
2026-03-29 22:45:48 +0000

    Update server.js and index.html to create interactive calculator

--- 2.2P/server.js
+++ 2.2P/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-// Serve static files from public folder
 app.use(express.static('public'));
 
-// Basic endpoint
-app.get('/', (req, res) => {
-    res.send('Server is running 🚀');
+app.get("/addTwoNumbers", (req, res) => {
+    // We use req.query to get values from the URL
+    const n1 = parseInt(req.query.number1);
+    const n2 = parseInt(req.query.number2);
+
+    if (isNaN(n1) || isNaN(n2)) {
+        return res.status(400).json({status: 400, message: "Invalid numbers"});
+    }
+
+    res.json({status: 200, data: n1 + n2});
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
